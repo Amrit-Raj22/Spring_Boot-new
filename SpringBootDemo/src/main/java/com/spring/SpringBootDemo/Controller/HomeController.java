cdd5472298899513a6a4541089d245bbdf737da6
Amrit Raj <98614443+Amrit-Raj22@users.noreply.github.com>
2022-03-30 14:55:32 +0530

    Add files via upload
    Updated

---
+++ SpringBootDemo/src/main/java/com/spring/SpringBootDemo/Controller/HomeController.java
@@ -0,0 +1,11 @@
+package com.spring.SpringBootDemo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "Hello Visitor";
+	}
+}
---
+++ SpringBootDemo/src/main/java/com/spring/SpringBootDemo/Controller/TaskCotroller.java
@@ -0,0 +1,31 @@
+package com.spring.SpringBootDemo.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TaskCotroller {
+	
+	@GetMapping("/task")
+	public String task() {
+		return "Task Demonstration";
+	}
+	
+	@GetMapping("/task/showMyInfo")
+	public String detail() {
+		return "Name:Amrit\n "
+				+ "CITY:city\n "
+				+ "State:Bihar\n "
+				+ "DreamJob:Sleeping";
+	}
+	
+	@GetMapping("/task/showMyHobby")
+	public List<String> hobbies() {
+		return List.of("EAT","SLEEP","REPEAT");
+	}
+	
+	
+	
+}
